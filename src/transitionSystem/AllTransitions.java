256eeb5f743822fdd2d83cb7fcf3f2e6974b9b31
tschneichel <tschneichel@uni-koblenz.de>
2019-01-12 11:59:30 +0100

    test_commit

--- src/transitionSystem/AllTransitions.java
+++ src/transitionSystem/AllTransitions.java
@@ -1,5 +1,9 @@
 package transitionSystem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AllTransitions {
+	public Map<String,Transition> nameToTransition = new HashMap<String, Transition>();
 
 }
--- src/transitionSystem/Transition.java
+++ src/transitionSystem/Transition.java
@@ -9,5 +9,5 @@ public class Transition {
 //	public Map<String,Transition> previousTransitions = new HashMap<String, Transition>();
 	// Probably unneccessary?
 	public int count;
-	
+	// test
 }
