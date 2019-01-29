34ccfe39f73e69381aa41ee6dfdb9c82977c35e8
tschneichel <tschneichel@uni-koblenz.de>
2019-01-29 16:42:49 +0100

    New class for output elements

---
+++ src/transitionData/Recommendation.java
@@ -0,0 +1,20 @@
+package transitionData;
+
+public class Recommendation {
+	public String nextTransition;
+	public Float probability;
+	public String getNextTransition() {
+		return nextTransition;
+	}
+	public void setNextTransition(String nextTransition) {
+		this.nextTransition = nextTransition;
+	}
+	public Float getProbability() {
+		return probability;
+	}
+	public void setProbability(Float probability) {
+		this.probability = probability;
+	}
+	
+	
+}
