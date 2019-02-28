599b9b10e4db55e6ed8a7811b6a656ad843cca15
tschneichel <tschneichel@uni-koblenz.de>
2019-02-28 16:55:25 +0100

    deleted no longer necessary classes and references to them

--- src/transitionData/State.java
+++ src/transitionData/State.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import pfa_elements.transitionOld;
 import recommendationData.Recommendation;
 
 public class State implements Serializable {
